d8adf02df95108c65cdfdee03f3a47e92ae3f15f
Aeronautiac <110267915+Aeronautiac@users.noreply.github.com>
2023-01-30 15:03:38 -0500

    TierSorter moved

--- gameItems.hpp
+++ gameItems.hpp
@@ -52,29 +52,4 @@ class GameItems {
 };
 GameItems gameItems;
 
-template<typename T> class TierSorter {
-  
-  private:
-    vector<vector<T>> items;
-
-  public:
-    void addItem(int tier, T item) {
-      if (items.size() < tier+1) {
-        items.resize(tier+1);
-      }
-      items[tier] = vector<T>{};
-      items[tier].push_back(item);
-    }
-
-    T getRandomItemOfTier(int tier) {
-      vector<T> tierItems;
-      try {
-        tierItems = items[tier];
-      } catch (...) {
-        tierItems = items.back();
-      }
-      return tierItems[rand() % tierItems.size()];
-    }
-};
-
 #endif
